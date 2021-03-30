476b8cd1b718a446991b43a2d27120f7f1954168
KalebMcElyea <kalebmac@live.com>
2021-03-30 14:59:14 -0600

    removed lib

--- pack.client/src/main.js
+++ pack.client/src/main.js
@@ -6,10 +6,9 @@ import router from './router'
 import bootstrap from 'bootstrap'
 import jquery from 'jquery'
 import popper from 'popper.js'
-import videobackground from 'vue-responsive-video-background-player'
 
 const root = createApp(App)
-root.component('VideoBackground', videobackground)
+
 registerGlobalComponents(root)
 
 root
