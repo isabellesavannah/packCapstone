17f3790d4d907aa395e5df82eb70b106e3e4ecf3
KalebMcElyea <kalebmac@live.com>
2021-03-30 16:48:23 -0600

    Prolife Page Modal Added

--- pack.client/src/pages/ProfilesPage.vue
+++ pack.client/src/pages/ProfilesPage.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="container-fluid">
-    <div class="row background1">
-      <div class="col-12 d-flex justify-content-center">
-        <video class="video" autoplay>
-          <source
-            :src="url"
-            type="video/mp4"
-          />
-        </video>
-      </div>
+    <div class="row background1 d-flex justify-content-center">
+      <div class="col-12">
+        <!-- start modal -->
+        <button type="button" class="position-absolute btn btn-demo text-light" style="top: 3%; left: 3%" data-toggle="modal" data-target="#myModal">
+          <i class="fas fa-dog fa-2x"></i> <i class="fas fa-search"></i>
+        </button>
+        <!-- modal pop up -->
+        <div class="modal left fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span class="text-danger mr-1" aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">
+                  Go Fetch
+                </h4>
+              </div>
+
+              <div class="modal-body">
+                <ul class="pb-2">
+                  <li>Filter by Item 1</li>
+                  <li>Filter by Item 2</li>
+                  <li>Filter by Item 3</li>
+                  <li>Filter by Item 4</li>
+                  <li>Filter by Item 5</li>
+                </ul>
+              </div>
+            </div><!-- modal-content -->
+          </div><!-- modal-dialog -->
+        </div><!-- modal -->
+      </div><!-- container -->
+      <!-- end modal -->
+      <!-- start video -->
+      <video class="video" autoplay>
+        <source
+          :src="url"
+          type="video/mp4"
+        />
+      </video>
+      <!-- end video -->
     </div>
+    <!-- start profiles -->
     <div class="row">
       <div class="col-12">
         <h1>Dog Profiles go Here =></h1>
       </div>
     </div>
+    <!-- end profiles -->
   </div>
 </template>
 
@@ -40,4 +74,53 @@ export default {
 /* *{
   border: 1px dotted red;
 } */
+/* start modal */
+/*******************************
+* MODAL AS LEFT/RIGHT SIDEBAR
+* Add "left" or "right" in modal parent div, after class="modal".
+* Get free snippets on bootpen.com
+*******************************/
+.modal.left .modal-dialog {
+margin: auto;
+width: -250px;
+height: 100%;
+-webkit-transform: translate3d(0%, 0, 0);
+    -ms-transform: translate3d(0%, 0, 0);
+    -o-transform: translate3d(0%, 0, 0);
+       transform: translate3d(0%, 0, 0);
+}
+
+.modal.left .modal-content {
+height: 100%;
+width: 40%;
+overflow-y: auto;
+}
+
+/* .modal.left .modal-body {
+padding: 15px 15px 80px;
+} */
+
+/*Left*/
+.modal.left.fade .modal-dialog{
+left: -250px;
+-webkit-transition: opacity 0.3s linear, left 0.3s ease-out;
+  -moz-transition: opacity 0.3s linear, left 0.3s ease-out;
+    -o-transition: opacity 0.3s linear, left 0.3s ease-out;
+           transition: opacity 0.3s linear, left 0.3s ease-out;
+}
+
+.modal.left.fade.in .modal-dialog{
+left: 20;
+}
+
+/* ----- MODAL STYLE ----- */
+.modal-content {
+border-radius: 0;
+border: none;
+}
+
+.modal-header {
+border-bottom-color: #EEEEEE;
+background-color: #FAFAFA;
+}
 </style>
