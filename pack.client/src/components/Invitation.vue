9e62c38e7fe21d79fe63cbea6e6a7f51cc7c7be8
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-04-01 15:06:09 -0600

    added picture to invite

--- pack.client/src/components/Invitation.vue
+++ pack.client/src/components/Invitation.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="Invitation">
-    <div class="card m-2 d-flex justify-content-center" style="width 100%">
-      <!-- <img class="card-img-top" :src="userProfile.img"> -->
+    <div class="card m-2 d-flex justify-content-center">
+      <img class="card-img-top" :src="invitationProp.inviteId.creator.img">
       <div class="card-body">
         <h5 class="card-title text-center">
           {{ invitationProp.inviteId.creator.petName }}
         </h5>
         <button class="btn btn-danger" @click="deleteInvitation">
           Decline
-        </button> <button class="btn btn-success" @click="acceptInvitation">
+        </button> <button class="float-right btn btn-success" @click="acceptInvitation">
           Accept
         </button>
       </div>
@@ -46,5 +46,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.pic{
+  display: inline-block;
+  padding: 8px;
+  background: linear-gradient(130deg, #aa50e2, #ffa10a);
+  object-fit: cover;
+  margin: auto;
+  border-radius: 50%;
+  width: 150px;
+  height:150px;
+}
+.transparent{
+   background-color: rgba(255, 255, 255, 0.438);
+}
+
+.card{
+  box-shadow: 2px 6px 8px 2px #aa50e2;
+  transition: 0.3s ease-in-out;
+  width: 100%;
+  height: 85%;
+}
+
+.card:hover{
+   box-shadow: 2px 8px 16px 2px aqua;
+}
 
 </style>
